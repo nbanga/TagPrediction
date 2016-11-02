b2a6d4be4acc79cd2120e7d3c75e6e2d0d47690e
Nomchin Banga <Nomchin@Nomchins-MacBook-Pro.local>
2016-11-02 05:23:55 -0400

    tf-idf

---
+++ Code/model/tf_idf.py
@@ -0,0 +1,84 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+import json
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.tag import pos_tag
+from nltk.stem.porter import PorterStemmer
+import numpy
+import math
+import csv
+
+
+path = '../../Data/x_vector'
+labels = '../../Data/all_labels.json'
+tokens = '../../Data/tokenized_data.json'
+
+
+#for subdir, dirs, files in os.walk(path):
+#    for file in files:
+        #file_path = subdir + os.path.sep + file
+
+word_dict = {}
+X = []
+
+file = open(tokens,'r')
+data = json.load(file)
+
+index = 0
+for row in data:
+    #x = {'freq': {}, 'tf': {}, 'idf': {}, 'tf-idf': {}, 'tokens': []}
+    s = set(row["title"])
+    for title in s:
+        if title not in word_dict.keys():
+            word_dict[title] = [index, 1]
+            index += 1
+        else :
+            word_dict[title][1] = word_dict[title][1] + 1
+
+d = len(data)
+l = len(word_dict)
+
+word_dict_tf = dict(map(lambda (k,(u,v)): (k, (u, math.log(d/float(v)))), word_dict.iteritems()))
+
+X = [[0.0]*l]*d
+for i in range(d):
+    p = data[i]["title"]
+    temp = [0.0]*l
+    for word in p:
+        temp[word_dict_tf[word][0]] = word_dict_tf[word][1]*(1.0*p.count(word)/len(p))
+    X[i] = Y
+
+label_file = open(labels,'r')
+label = json.load(label_file)
+
+Y = [[-1]*100]*d
+for i in range(d):
+    p = data[i]["tags"]
+    temp = [-1]*100
+    for word in p:
+        for sublist in label:
+            if sublist[0] == word:
+                temp[label.index(sublist)] = 1
+                break
+    Y[i] = temp
+    print (Y[i].count(1))
+
+label_file.close()
+
+with open(path + "/X.csv", "wb") as f:
+    writer = csv.writer(f)
+    writer.writerows(X)
+f.close()
+
+with open(path + "/Y.csv", "wb") as f:
+    writer = csv.writer(f)
+    writer.writerows(Y)
+f.close()
+
+
+
+
+
+
+
+
--- Code/util/split_by_label.py
+++ Code/util/split_by_label.py
@@ -2,27 +2,31 @@ import json
 import os,sys
 
 filepath = "../../Data/x_vector"
-tokenpath = "../../Data/tokenized"
+tokenpath = "../../Data/tokenized_data.json"
+path = "../../Data/all_labels.json"
 
 def split_by_label(label,num_rows):
     label_filepath = filepath + "/" + label + ".json"
     feed = []
-    for file in os.listdir(tokenpath):
-        curr_file = open(file,'r')
-        data = json.load(curr_file)
-        for row in data:
-            if label in row["tags"]:
-                feed.append(row)
-                if len(feed)==num_rows:
-                    break
-        if len(feed)==num_rows:
-            break
+    curr_file = open(tokenpath,'r')
+    data = json.load(curr_file)
+    for row in data:
+        if label in row["tags"]:
+            feed.append(row)
+            if len(feed)==num_rows:
+                break
 
+    #print(feed)
     label_file = open(label_filepath,'w')
-    json.dumps(feed,label_file)
+    json.dump(feed,label_file)
+    label_file.close()
 
 def main():
-    split_by_label(label,num_rows)
+    file = open(path,'r')
+    data = json.load(file)
+    for label in data:
+        print label
+        split_by_label(label[0][0],100)
 
 if __name__=='__main__':
     main()
